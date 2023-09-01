f7681cc166da05b62949a30f19afc6b2e98b6b0e
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-08-31 21:04:26 -0400

    finished #00074 search-a-2d-matrix
    commit 5d2861350fab37ef575c89bafcfacb15241f50a0
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 21:03:34 2023 -0400
    
        marked #00074 finished
        On branch search-a-2d-matrix
        Changes to be committed:
        	renamed:    problems/!00074/Cargo.toml -> problems/00074/Cargo.toml
        	renamed:    problems/!00074/src/lib.rs -> problems/00074/src/lib.rs
    
    commit b230713714f2dc44a3824a56ff0afb87283be81f
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 21:00:13 2023 -0400
    
        fix out-of-bounds issue
        On branch search-a-2d-matrix
        Changes to be committed:
        	modified:   src/lib.rs
    
    commit 403bf0246681db9cf34feca61506d8fc6da5dc2d
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 20:55:45 2023 -0400
    
        tentative first solution
        On branch search-a-2d-matrix
        Changes to be committed:
        	modified:   src/lib.rs
    
    commit 25e8e6079e7b2bd09d81812d118b16bff8f477f6
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 20:41:58 2023 -0400
    
        started problem #00074
        On branch search-a-2d-matrix
        Changes to be committed:
        	new file:   Cargo.toml
        	new file:   src/lib.rs
    
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   problems/00074/Cargo.toml
    	new file:   problems/00074/src/lib.rs

---
+++ problems/00074/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        // Search rows by the last element in the row, if found return true. If Err, the value
+        // contained will tell us the row where the element to be found would be in.
+        match matrix.binary_search_by_key(&target, |row| *row.last().unwrap()) {
+            Ok(_) => true,
+            // Search the row given by Err() for the value.
+            Err(i) => {
+                if i < matrix.len() {
+                    matrix[i].binary_search(&target).is_ok()
+                } else {
+                    // If i >= matrix.len(), it is out of bounds and there is nothing to search.
+                    false
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn first_matrix_tests_true() {
+        let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
+        assert!(Solution::search_matrix(matrix, 3));
+    }
+
+    #[test]
+    fn second_matrix_tests_false() {
+        let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
+        assert!(!Solution::search_matrix(matrix, 13));
+    }
+
+    #[test]
+    fn third_matrix_tests_false() {
+        let matrix = vec![vec![1]];
+        assert!(!Solution::search_matrix(matrix, 2));
+    }
+}
