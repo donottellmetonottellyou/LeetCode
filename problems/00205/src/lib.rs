5d962a6b88cbe22cc17e0749aa4d9e8d4cc95de2
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-08-31 20:32:38 -0400

    finished #00205 isomorphic-strings
    commit 2d12a1cedeb99b962db373432d16c35edc1896cc
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 20:31:27 2023 -0400
    
        marked #00205 completed
        On branch isomorphic-strings
        Changes to be committed:
        	renamed:    problems/!00205/Cargo.toml -> problems/00205/Cargo.toml
        	renamed:    problems/!00205/src/lib.rs -> problems/00205/src/lib.rs
    
    commit 4f4dfb65d0e58ddf6c30f3b6da19de8251d8c375
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 20:27:41 2023 -0400
    
        add tests
        On branch isomorphic-strings
        Changes to be committed:
        	modified:   problems/!00205/src/lib.rs
    
    commit 864b6f063f6d962535a357817a7a1eed09fbe9df
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 20:22:22 2023 -0400
    
        finished first attempt at problem
        On branch isomorphic-strings
        Changes to be committed:
        	modified:   problems/!00205/src/lib.rs
    
    commit 5dc5c37af41c2c759d19b98784f5ff3399f414f5
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Thu Aug 31 20:00:55 2023 -0400
    
        setup problem #00205
        On branch isomorphic-strings
        Changes to be committed:
        	new file:   problems/!00205/Cargo.toml
        	new file:   problems/!00205/src/lib.rs

---
+++ problems/00205/src/lib.rs
@@ -0,0 +1,64 @@
+pub struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        // We assume s.len() == t.len().
+
+        // Turn s and t into mapped patterns.
+        let s = Self::map_bytes(&s);
+        let t = Self::map_bytes(&t);
+
+        // map_bytes() will ensure that if s and t are isomorphic, the patterns will be the same.
+        s.into_iter()
+            .zip(t)
+            .all(|(s_byte, t_byte)| s_byte == t_byte)
+    }
+
+    fn map_bytes(s: &str) -> Vec<u8> {
+        // We turn the &str into a pattern where the first character encountered is 0, the second 1,
+        // the third 2, etc. If we find a byte that already was encountered, we use the byte mapping
+        // it previously was.
+
+        // Keep track of the nth character
+        let mut n: u8 = 0;
+        // Keep track of previous characters we have already encountered.
+        let mut mappings: HashMap<u8, u8> = HashMap::with_capacity(128);
+        let mut mapped: Vec<u8> = Vec::with_capacity(s.len());
+
+        for byte in s.bytes() {
+            if let Some(mapped_byte) = mappings.get(&byte) {
+                // If we already encountered a byte, push the mapped byte into mapped.
+                mapped.push(*mapped_byte);
+            } else {
+                // If we haven't encountered the byte, record it as the nth byte and increment n.
+                mapped.push(n);
+                mappings.insert(byte, n);
+                n += 1;
+            }
+        }
+
+        mapped
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn egg_and_add_are_isomorphic() {
+        assert!(Solution::is_isomorphic("egg".into(), "add".into()));
+    }
+
+    #[test]
+    fn foo_and_bar_are_not_isomorphic() {
+        assert!(!Solution::is_isomorphic("foo".into(), "bar".into()));
+    }
+
+    #[test]
+    fn paper_and_title_are_isomorphic() {
+        assert!(Solution::is_isomorphic("paper".into(), "title".into()));
+    }
+}
