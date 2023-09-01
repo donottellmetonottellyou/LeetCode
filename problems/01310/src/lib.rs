2cec36c47ee0f86dc0d625af7236050d8dacb4b7
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-01 17:26:58 -0400

    finished problem #01310 xor-queries-of-a-subarray 12ms/4.1MB
    I enjoyed this problem and learned about xor prefix sums!
    
    Squashed commit of the following:
    
    commit 102cc1f0cca136cf68a2a8d015d8db8abd5f3a22
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 1 17:26:14 2023 -0400
    
        marked as complete
    
    commit 9d92040e3dc14b861a520e1ff54d105d3242861f
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 1 17:24:35 2023 -0400
    
        improved speed 275ms -> 12ms leetcode benchmark
    
    commit 3b271acbd4fe62b269ecc45cece80dfc9e0ce5c2
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 1 17:04:44 2023 -0400
    
        tentative solution for #01310
    
    commit b72c73880adc95ae8197bd5ec45982d770edac00
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 1 16:46:19 2023 -0400
    
        starting work on problem #01310
        xor-queries-of-a-subarray
        On branch xor-queries-of-a-subarray
        Changes to be committed:
        	new file:   Cargo.toml
        	new file:   src/lib.rs

---
+++ problems/01310/src/lib.rs
@@ -0,0 +1,45 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn xor_queries(arr: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let prefix_sums = Self::calculate_prefix_sums(arr);
+
+        queries
+            .into_iter()
+            .map(|query| (query[0] as usize, query[1] as usize + 1))
+            .map(|(left_bits, right_bits)| prefix_sums[left_bits] ^ prefix_sums[right_bits])
+            .collect()
+    }
+
+    fn calculate_prefix_sums(arr: Vec<i32>) -> Vec<i32> {
+        let mut prefix_sums = Vec::with_capacity(arr.len() + 1);
+        prefix_sums.push(0);
+
+        for num in arr {
+            prefix_sums.push(prefix_sums.last().unwrap() ^ num);
+        }
+
+        prefix_sums
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn xor_queries_1() {
+        let arr = vec![1, 3, 4, 8];
+        let queries = vec![vec![0, 1], vec![1, 2], vec![0, 3], vec![3, 3]];
+
+        assert_eq!(Solution::xor_queries(arr, queries), vec![2, 7, 14, 8]);
+    }
+
+    #[test]
+    fn xor_queries_2() {
+        let arr = vec![4, 8, 2, 10];
+        let queries = vec![vec![2, 3], vec![1, 3], vec![0, 0], vec![0, 3]];
+
+        assert_eq!(Solution::xor_queries(arr, queries), vec![8, 0, 4, 4]);
+    }
+}
