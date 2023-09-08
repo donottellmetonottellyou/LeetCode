7cccc2b95e40a43cc89a61bb33f2992c31a034b2
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-08 01:29:30 -0400

    finished number-of-valid-clock-times 0ms/2MB
    Squashed commit of the following:
    
    commit ca2220da99639cade2da2a906d540b6f062ab936
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 8 01:28:22 2023 -0400
    
        finish problem #02437 number-of-valid-clock-times
        0ms/2MB
    
    commit 9df8b86bbbdf0ec840c2c01b841d5ba28c867b7f
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 8 01:26:39 2023 -0400
    
        first solution
    
    commit a2977fb6be9f8c71bab55535de724c6870b1c89f
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Fri Sep 8 01:04:17 2023 -0400
    
        started problem #02437 number-of-valid-clock-times

---
+++ problems/02437/src/lib.rs
@@ -0,0 +1,42 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn count_time(time: String) -> i32 {
+        let hour_combinations = match (time.as_bytes()[0], time.as_bytes()[1]) {
+            (b'?', b'?') => 24,
+            (b'?', b'0'..=b'3') => 3,
+            (b'?', _) => 2,
+            (b'0'..=b'1', b'?') => 10,
+            (_, b'?') => 4,
+            (_, _) => 1,
+        };
+        let minute_combinations = match (time.as_bytes()[3], time.as_bytes()[4]) {
+            (b'?', b'?') => 60,
+            (b'?', _) => 6,
+            (_, b'?') => 10,
+            (_, _) => 1,
+        };
+
+        hour_combinations * minute_combinations
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn counts_time_1() {
+        assert_eq!(Solution::count_time("?5:00".into()), 2);
+    }
+
+    #[test]
+    fn counts_time_2() {
+        assert_eq!(Solution::count_time("0?:0?".into()), 100);
+    }
+
+    #[test]
+    fn counts_time_3() {
+        assert_eq!(Solution::count_time("??:??".into()), 1440);
+    }
+}
