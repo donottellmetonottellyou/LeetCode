41399513dbb6bfdabaecb1f0c0ba6459a63a7f1b
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-13 02:18:48 -0400

    finished divide-array-into-equal-parts 3ms/2.1MB
    Squashed commit of the following:
    
    commit 1669e403a97d8b5762ddc3ff7fcd705e8860ddf2
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 02:17:32 2023 -0400
    
        finished #02206 divide-array-into-equal-parts
        3ms/2.1MB
    
    commit 1b532934ac3c9c634d413bdf00691abd17399739
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 02:15:38 2023 -0400
    
        solution without hashmap/set
    
    commit a3f340cc658ba2c22347ccb1b8d0901a52ae9c8a
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 02:03:10 2023 -0400
    
        better optimizations
    
        I made the HashMap into a HashSet, checked in the beginning if the array
        was odd, and kept track of unpaired numbers instead of keeping track of
        all numbers
    
    commit 11e0efc63f09bdb2e4b0254406fb1e6d6a46397e
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 01:54:58 2023 -0400
    
        go back to previous solution as it wasn't slower
    
    commit 3c025a2420295f8bcae3819d4c93cce8d201d4d3
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 01:46:29 2023 -0400
    
        fixed bug where we didn't loop through heap sorted
    
    commit 4e89152a9c41a1c9eb4cac787197b94130a444ee
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 01:36:27 2023 -0400
    
        maybe optimized
    
    commit f04eed6bf856c37598f4d63e38f6e99666e4048e
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 01:22:43 2023 -0400
    
        first solution
    
    commit 5e8c4b904a3f987538c910eca2bb8b7a54d7d8f2
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 13 01:12:57 2023 -0400
    
        started #02206 divide-array-into-equal-parts

---
+++ problems/02206/src/lib.rs
@@ -0,0 +1,49 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn divide_array(nums: Vec<i32>) -> bool {
+        if nums.len() % 2 == 1 {
+            return false;
+        }
+
+        let mut not_paired = Vec::new();
+        for num in nums {
+            if let Some(unpaired_at) = not_paired.iter().position(|unpaired| *unpaired == num) {
+                not_paired.remove(unpaired_at);
+            } else {
+                not_paired.push(num);
+            }
+        }
+
+        not_paired.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn finds_array_divisible_1() {
+        let nums = vec![3, 2, 3, 2, 2, 2];
+
+        assert!(Solution::divide_array(nums));
+    }
+
+    #[test]
+    fn finds_array_divisible_2() {
+        let nums = vec![
+            9, 4, 18, 3, 2, 6, 18, 15, 7, 15, 6, 4, 15, 14, 7, 4, 15, 4, 3, 17, 9, 13, 13, 12, 2,
+            14, 12, 17,
+        ];
+
+        assert!(Solution::divide_array(nums));
+    }
+
+    #[test]
+    fn finds_array_indivisible_1() {
+        let nums = vec![1, 2, 3, 4];
+
+        assert!(!Solution::divide_array(nums));
+    }
+}
