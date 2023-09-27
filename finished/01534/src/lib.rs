20425883729d5da53ea4e12dd4dcafb205614d70
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-27 17:32:17 -0400

    finished count-good-triplets 2ms/2.1MB
    Squashed commit of the following:
    
    commit 2d41813d08aee8b7e50226517b1ab954e5c9d540
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 27 17:31:00 2023 -0400
    
        finished #01534 count-good-triplets
        apparently it was good enough
        2ms/2.1MB
    
    commit 4287bc77d58dfde1dfeab260a8e7eb18035cb73d
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 27 17:28:59 2023 -0400
    
        first (probably slow) solution
    
    commit 6cf005e2d0bebc08f533ca67b157ac2e311f12fb
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Sep 27 17:13:36 2023 -0400
    
        started #01534 count-good-triplets

---
+++ finished/01534/src/lib.rs
@@ -0,0 +1,47 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
+        let mut count = 0;
+
+        for i in 0..arr.len() {
+            for j in (i + 1)..arr.len() {
+                if (arr[i] - arr[j]).abs() > a {
+                    continue;
+                }
+                for k in (j + 1)..arr.len() {
+                    if (arr[j] - arr[k]).abs() <= b && (arr[i] - arr[k]).abs() <= c {
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn counts_good_triplets_1() {
+        let arr = vec![3, 0, 1, 1, 9, 7];
+        let a = 7;
+        let b = 2;
+        let c = 3;
+
+        assert_eq!(Solution::count_good_triplets(arr, a, b, c), 4);
+    }
+
+    #[test]
+    fn counts_good_triplets_2() {
+        let arr = vec![1, 1, 2, 2, 3];
+        let a = 0;
+        let b = 0;
+        let c = 1;
+
+        assert_eq!(Solution::count_good_triplets(arr, a, b, c), 0);
+    }
+}
