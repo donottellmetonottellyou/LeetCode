c764a58053567fa6e96cb8e32752b11a9c1580ea
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-11-01 11:46:34 -0400

    finished fizz-buzz 2ms/2.6MB Squashed commit of the following:
    commit 9481bd42063ff325a4d3cf1a00195114afb4cd21
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Nov 1 11:45:19 2023 -0400
    
        finished #00412 fizz-buzz
        2ms/2.6MB
    
    commit 77569344f6442f2bcdde9daeab4a5fd200c4b779
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Nov 1 11:42:14 2023 -0400
    
        Revert "maybe optimized solution"
    
        This reverts commit 89cee059eb696d34e22aa38d61bbd6a82d8d36d8.
    
        optimize for speed rather than memory usage
    
    commit 89cee059eb696d34e22aa38d61bbd6a82d8d36d8
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Nov 1 11:40:19 2023 -0400
    
        maybe optimized solution
    
    commit 624a931530817b99e2238033596a183bef2160f0
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Nov 1 11:35:59 2023 -0400
    
        add tests
    
    commit b0e8fda264a917d9468e2f83611fd07d481f6325
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Nov 1 11:28:40 2023 -0400
    
        first solution
    
    commit 4c8ef4a8595378efb7ee1cb413ae6acab4d17b3a
    Author: Jade Lynn Masker <donottellmetonottellyou@gmail.com>
    Date:   Wed Nov 1 11:16:59 2023 -0400
    
        setup #00412 fizz-buzz

---
+++ finished/00412/src/lib.rs
@@ -0,0 +1,59 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn fizz_buzz(n: i32) -> Vec<String> {
+        let mut answer = Vec::with_capacity(n as usize);
+
+        for n in 1..=n {
+            // At most will contain "FizzBuzz", which is 8 bytes long, unless n > 99999999.
+            let mut string = String::with_capacity(8);
+
+            if n % 3 == 0 {
+                string.push_str("Fizz");
+            }
+            if n % 5 == 0 {
+                string.push_str("Buzz");
+            }
+            if string.is_empty() {
+                string.push_str(&n.to_string())
+            }
+
+            answer.push(string);
+        }
+
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn to_owned_strings(slices: Vec<&str>) -> Vec<String> {
+        slices.iter().map(|s| s.to_string()).collect()
+    }
+
+    #[test]
+    fn fizz_buzzes_1() {
+        let solution = to_owned_strings(vec!["1", "2", "Fizz"]);
+
+        assert_eq!(solution, Solution::fizz_buzz(3));
+    }
+
+    #[test]
+    fn fizz_buzzes_2() {
+        let solution = to_owned_strings(vec!["1", "2", "Fizz", "4", "Buzz"]);
+
+        assert_eq!(solution, Solution::fizz_buzz(5));
+    }
+
+    #[test]
+    fn fizz_buzzes_3() {
+        let solution = to_owned_strings(vec![
+            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13",
+            "14", "FizzBuzz",
+        ]);
+
+        assert_eq!(solution, Solution::fizz_buzz(15));
+    }
+}
